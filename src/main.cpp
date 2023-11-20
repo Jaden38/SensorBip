e1b0eb80c3e9b5eab0a493fe502bc97a531fa068
Jaden38 <damien.nithard@gmail.com>
2023-11-20 11:41:23 +0100

    Commit bip et creation fichier functions.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUZZER_PIN 12
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void beep(int pin, int frequency, int duration)
+{
+  tone(pin, frequency, duration);
+  delay(duration + 50);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+
+  // for (int i = 0; i < 1; ++i)
+  // {
+  //   beep(BUZZER_PIN, 5000, 500);
+  //   beep(BUZZER_PIN, 10000, 500);
+  //   beep(BUZZER_PIN, 2000, 500);
+  //   beep(BUZZER_PIN, 16000, 500);
+  //   beep(BUZZER_PIN, 20000, 500);
+  //   delay(1000);
+  // }
 }
